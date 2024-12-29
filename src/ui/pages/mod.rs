2913a7cd7c3cdf479eda8365727d988b0f576b10
Ivan <ivan.pinchuk.01@gmail.com>
2024-12-29 17:02:31 -0600

    added navigator and main loop

--- src/ui/pages/mod.rs
+++ src/ui/pages/mod.rs
@@ -1,3 +1,4 @@
+use std::any::Any;
 use std::rc::Rc;
 
 use itertools::Itertools;
@@ -13,6 +14,7 @@ use page_helpers::*;
 pub trait Page {
     fn draw_page(&self) -> Result<()>;
     fn handle_input(&self, input: &str) -> Result<Option<Action>>;
+    fn as_any(&self) -> &dyn Any;
 }
 
 pub struct HomePage {
@@ -58,6 +60,10 @@ impl Page for HomePage {
             }
         }
     }
+
+    fn as_any(&self) -> &dyn Any {
+        self
+    }
 }
 
 pub struct EpicDetail {
@@ -82,7 +88,7 @@ impl Page for EpicDetail {
 
         println!();
 
-        println!("---------------------------- STORIES ----------------------------");
+        println!("---------------------------- STORIES -----------------------------");
         println!("     id     |               name               |      status      ");
 
         let stories = &db_state.stories;
@@ -119,6 +125,10 @@ impl Page for EpicDetail {
                 Ok(None)
             }
         }
+    }
+
+    fn as_any(&self) -> &dyn Any {
+        self
     }
 }
 
@@ -157,6 +167,10 @@ impl Page for StoryDetail {
             "d" => Ok(Some(Action::DeleteStory { epic_id: self.epic_id, story_id: self.story_id })),
             _ => Ok(None)
         }
+    }
+
+    fn as_any(&self) -> &dyn Any {
+        self
     }
 }
 
